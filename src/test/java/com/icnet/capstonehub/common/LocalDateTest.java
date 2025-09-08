33625617faf2732c488a540cd672c2caa80b5e6f
lilamaris <lilamaris.dev@gmail.com>
2025-09-08 12:23:00 +0900

    test: add LocalDate test

---
+++ src/test/java/com/icnet/capstonehub/common/LocalDateTest.java
@@ -0,0 +1,19 @@
+package com.icnet.capstonehub.common;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LocalDateTest {
+
+    @Test
+    public void localdateCalc() {
+        LocalDate date = LocalDate.of(2022, 12, 31);
+
+        assertEquals(LocalDate.of(2023, 1, 2), date.plusDays(2));
+    }
+}
+
